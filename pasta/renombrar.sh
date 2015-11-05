22d743a7e57cce4d05f7e496fce415fee704e8bc
ShavelyPusey <chsinisterra@uan.edu.co>
2015-11-05 11:44:10 -0500

    DataImg

---
+++ pasta/renombrar.sh
@@ -0,0 +1,16 @@
+ #!/bin/bash
+ ################################
+ # renombrar.sh                 #
+ # script renombramiento masivo #
+ # numerando desde 1            #
+ ################################
+ cont=0
+ nombre="s"
+ for picture in `ls *.png`
+ do
+ ((cont=$cont+1))
+ nuevonombre=$nombre$cont
+ echo "Renombrando... $picture"
+ echo "a $nuevonombre.png"
+ mv $picture $nuevonombre.png
+ done
